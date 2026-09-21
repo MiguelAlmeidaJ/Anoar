48aec1f30c1d16f725dd910d648640eabeb4d415
Miguel Almeida <100483395+MiguelAlmeidaJ@users.noreply.github.com>
2026-09-21 18:58:04 -0300

    feat: add site contact configuration

---
+++ config/site.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'brand' => 'Anoar',
+    'phone_display' => '+55 (32) 98407-5039',
+    'phone_whatsapp' => '5532984075039',
+    'instagram_url' => 'https://www.instagram.com/grupoanoar/',
+    'instagram_handle' => '@grupoanoar',
+];
